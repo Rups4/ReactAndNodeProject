57713c87c6bb3f68a9d3fd0c2830a528b743ee7f
unknown <rupali.naik4@gmail.com>
2021-01-20 22:47:55 +0530

    uncommented

--- server/app.js
+++ server/app.js
@@ -27,6 +27,7 @@ app.use("/detailsApi", detailsAPIRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next(createError(404));
 });
